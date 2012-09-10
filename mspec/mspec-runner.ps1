001c9c856845cb26faf658802dd9efe1d2f05fea
Kevin Kemp <kmkemp11@gmail.com>
2012-09-10 15:14:40 -0500

    mspec

---
+++ mspec/mspec-runner.ps1
@@ -0,0 +1,1 @@
+C:\dev\HPP\packages\Machine.Specifications.0.5.8\tools\mspec-clr4.exe --html .\testHtml.html --xml .\testXml.xml C:\dev\HPP\Services\Financial\ServiceU.Services.Financial.UnitTests\bin\Debug\ServiceU.Services.Financial.UnitTests.dll
